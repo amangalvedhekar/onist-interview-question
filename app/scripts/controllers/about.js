e546fb7748617a14fcc6b197f4abc5daab61ace3
Anirudh <anirudh.mangalvedhekar1989@gmail.com>
2017-09-10 14:12:08 -0400

    project setup using yo angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onistInterviewQuestionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the onistInterviewQuestionApp
+ */
+angular.module('onistInterviewQuestionApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onistInterviewQuestionApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the onistInterviewQuestionApp
+ */
+angular.module('onistInterviewQuestionApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
