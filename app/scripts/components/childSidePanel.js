3ecb4436c7dde868823291587164da179a51bf79
Anirudh <anirudh.mangalvedhekar1989@gmail.com>
2017-10-10 19:05:38 -0400

    all features added up

--- app/scripts/components/childSidePanel.js
+++ app/scripts/components/childSidePanel.js
@@ -2,11 +2,17 @@
  * Created by anirudh on 2017-09-18.
  */
 'use strict';
+
 function childSidePanelController(
-  $ngRedux
+  $ngRedux,
+  numberToWord
 ) {
   var $ctrl = this;
-  $ctrl.$onInit = function () {
+  $ctrl.$onChanges = function (change) {
+    console.log($ctrl.child.id, change.child.currentValue.id);
+
+    $ctrl.childNumberInWords = change.child.currentValue.id ?
+                               numberToWord.numberToOrdinal(change.child.currentValue.id): '';
     function mapStateToCtrl(state) {
       return {
         childrenInformation: state.childrenQuestion.childrenQuestion.data
@@ -20,33 +26,37 @@ function childSidePanelController(
 
   function removedChildStoreUpdate() {
     //ToDo: this wont work if an element is inserted in between
-    $ctrl.childrenInformation.splice($ctrl.childrenNumber-1, 1);
+    $ctrl.childrenInformation.splice($ctrl.childrenNumber - 1, 1);
     return {
       type: 'clearChildInformation',
       data: $ctrl.childrenInformation
     };
   }
+
   $ctrl.removeChildInformation = function () {
     $ngRedux
       .dispatch(
         removedChildStoreUpdate()
-    );
+      );
 
   };
 
 }
+
 var childSidePanelComponent = {
   templateUrl: 'views/child-side-panel.tpl.html',
   controller: 'childSidePanelController',
   bindings: {
+    child: '<',
     childrenInformation: '<',
     childrenNumber: '<'
   }
 };
 angular
   .module('onistInterviewQuestionApp')
-  .controller('childSidePanelController',[
+  .controller('childSidePanelController', [
     '$ngRedux',
+    'numberToWord',
     childSidePanelController
   ])
   .component('childSidePanel', childSidePanelComponent);
---
+++ app/scripts/services/numberToWord.js
@@ -0,0 +1,44 @@
+/**
+ * Created by anirudh on 2017-10-10.
+ */
+
+/*
+
+* */
+'use strict';
+
+function numberToWord() {
+
+  function numberToOrdinal(number) {
+    const child = ' child';
+    if (number === 0) {
+      return '';
+    }
+    var digits = [];
+    var ordinals = ['tenth', 'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eight', 'ninth'];
+    var units = [
+      '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+      'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',
+      'eighteen', 'nineteen'
+    ];
+    var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    while (number > 0) {
+      digits[digits.length] = number % 10;
+      number = parseInt(number / 10);
+    }
+    digits = digits.reverse();
+    if (digits.length === 1) {
+      return ordinals[digits[0]] + child;
+    } else {
+      return tens[digits[0]] + units[digits[1]] + child;
+    }
+
+  }
+
+  return {
+    'numberToOrdinal': numberToOrdinal
+  };
+
+}
+
+angular.module('onistInterviewQuestionApp').service('numberToWord', numberToWord);
